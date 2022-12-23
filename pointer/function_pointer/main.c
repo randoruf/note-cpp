73492447afce0ed10631faff77bd696a82db7ac5
上田光華 <masswie@foxmail.com>
2022-12-23 17:01:47 +0800

    Update main.c

--- pointer/function_pointer/main.c
+++ pointer/function_pointer/main.c
@@ -8,7 +8,7 @@ int64_t func_ptr = (int64_t) NULL;
 
 void func(void)
 {
-    printf("!!! func !!!\n");
+    printf("[ func ]: Hello World !\n");
 }
   
 void set_func_ptr(void *fp){
@@ -21,7 +21,7 @@ int main()
     set_func_ptr(func); 
     // printf("invoke func by C\n"); 
     // (*func_ptr)();
-    printf("invoke func by asm\n"); 
+    printf("invoke func by asm: \n\n"); 
     __asm__ __volatile__(
         "leaq _func_ptr(%rip), %r11\n"
         "movq (%r11), %r11\n"
