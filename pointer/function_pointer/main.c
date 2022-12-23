86e33ccc5fc5ccec96b0d64686d681ebc80e0da7
上田光華 <masswie@foxmail.com>
2022-12-23 17:01:10 +0800

    using a int64_t to store pointer

--- pointer/function_pointer/main.c
+++ pointer/function_pointer/main.c
@@ -1,8 +1,10 @@
+#include <stdint.h>
 #include <stdio.h>
 // A normal function with an int parameter
 // and void return type
 
-void (*func_ptr)(void) = NULL;
+// void (*func_ptr)(void) = NULL;
+int64_t func_ptr = (int64_t) NULL; 
 
 void func(void)
 {
@@ -10,15 +12,15 @@ void func(void)
 }
   
 void set_func_ptr(void *fp){
-    func_ptr = fp; 
+    func_ptr = (int64_t)fp; 
 }
 
 
 int main()
 {
     set_func_ptr(func); 
-    printf("invoke func by C\n"); 
-    (*func_ptr)();
+    // printf("invoke func by C\n"); 
+    // (*func_ptr)();
     printf("invoke func by asm\n"); 
     __asm__ __volatile__(
         "leaq _func_ptr(%rip), %r11\n"
